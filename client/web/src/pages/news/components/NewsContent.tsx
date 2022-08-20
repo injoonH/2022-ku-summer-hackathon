a0215269daa80102aaa709fc19712af77c493f17
cwhoaon <kcwkcw0802@gmail.com>
2022-08-21 07:57:15 +0900

    finish wifi connection example

--- client/web/src/pages/news/components/NewsContent.tsx
+++ client/web/src/pages/news/components/NewsContent.tsx
@@ -11,25 +11,62 @@ export const NewsContent: React.FC<NewsContentProps> = ({
   returnToPage,
 }) => {
   const [curPage, setcurPage] = React.useState(1);
+  const [wifiPwd, setwifiPwd] = React.useState("");
 
   let basePath = `src/assets/news/${title}/${title}-`;
   let endPath = ".png";
 
-  const interact = (e: any) => {
-    setcurPage(curPage + 1);
-    if (curPage == 7) {
-      returnToPage();
-    }
-  };
+  if (curPage == 6) {
+    setTimeout(() => {
+      setcurPage(curPage + 1);
+    }, 4000);
+  }
 
   return (
     <div className={styles.imageWrapper}>
       <img
         src={basePath + curPage + endPath}
         alt="card news content"
-        onClick={interact}
+        onClick={() => {
+          if (curPage == 7) returnToPage();
+        }}
       />
-      <div className={styles.touchArea}>hihi</div>
+      <input
+        type="text"
+        id={styles.touchArea4}
+        disabled={curPage != 4}
+        onChange={(e) => {
+          if (e.target.value === "123456") {
+            e.target.value = "";
+            setcurPage(curPage + 1);
+          }
+        }}
+      ></input>
+      <div
+        id={styles.touchArea1}
+        onClick={() => {
+          if (curPage == 1) setcurPage(curPage + 1);
+        }}
+      ></div>
+      <div
+        id={styles.touchArea2}
+        onClick={() => {
+          if (curPage == 2) setcurPage(curPage + 1);
+        }}
+      ></div>
+      <div
+        id={styles.touchArea3}
+        onClick={() => {
+          if (curPage == 3) setcurPage(curPage + 1);
+        }}
+      ></div>
+
+      <div
+        id={styles.touchArea5}
+        onClick={() => {
+          if (curPage == 5) setcurPage(curPage + 1);
+        }}
+      ></div>
     </div>
   );
 };
