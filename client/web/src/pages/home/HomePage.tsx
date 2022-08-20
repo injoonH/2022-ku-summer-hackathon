05a4a8b353a0c957148197825911db2257cacec4
Injoon Hwang <injoon0617@gmail.com>
2022-08-21 02:00:18 +0900

    feat: add mascot image in home page

--- client/web/src/pages/home/HomePage.tsx
+++ client/web/src/pages/home/HomePage.tsx
@@ -10,7 +10,15 @@ export const Home: React.FC = () => {
   const news = newsPreview.news[1];
 
   return (
-    <>
+    <div className={styles.container}>
+      <div className={styles.mascotWrapper}>
+        <span>영웅이</span>
+        <img
+          className={styles.mascot}
+          src="src/assets/mascot/mascot-front.png"
+          alt="mascot front"
+        />
+      </div>
       <ChatContainer>
         <Chat
           text="이런 수업은 어떠세요?"
@@ -27,6 +35,6 @@ export const Home: React.FC = () => {
         <Chat text="효정 님, 안녕하세요 무엇을 도와드릴까요?" posLeft />
         <Chat text="오늘 날씨 어때?" />
       </ChatContainer>
-    </>
+    </div>
   );
 };
