87ca2ca11ecc6e3559164698c06d6fed2b7fb245
cwhoaon <kcwkcw0802@gmail.com>
2022-08-20 18:26:24 +0900

    fix: jwt server response available

--- client/web/src/pages/meeting/MeetingPage.tsx
+++ client/web/src/pages/meeting/MeetingPage.tsx
@@ -13,39 +13,39 @@ import { ZoomMtg } from "@zoomus/websdk";
 const MeetingPage = () => {
   // document.getElementById("zmmtg-root").style.display = "none";
   // // setup your signature endpoint here: https://github.com/zoom/meetingsdk-sample-signature-node.js
-  // var signatureEndpoint = "http://localhost:4000";
-  // // This Sample App has been updated to use SDK App type credentials https://marketplace.zoom.us/docs/guides/build/sdk-app
-  // var sdkKey = "aI8eBOpQKSoulvEpydKIPNb8bsMWCWhuBaTG";
-  // var meetingNumber = "81754254237";
-  // var role = 0;
-  // var leaveUrl = "http://localhost:3000";
-  // var userName = "React";
-  // var userEmail = "";
-  // var passWord = "HACiz1";
+  var signatureEndpoint = "http://localhost:4000";
+  // This Sample App has been updated to use SDK App type credentials https://marketplace.zoom.us/docs/guides/build/sdk-app
+  var sdkKey = "aI8eBOpQKSoulvEpydKIPNb8bsMWCWhuBaTG";
+  var meetingNumber = "81754254237";
+  var role = 0;
+  var leaveUrl = "http://localhost:3000";
+  var userName = "React";
+  var userEmail = "";
+  var passWord = "HACiz1";
   // // pass in the registrant's token if your meeting or webinar requires registration. More info here:
   // // Meetings: https://marketplace.zoom.us/docs/sdk/native-sdks/web/client-view/meetings#join-registered
   // // Webinars: https://marketplace.zoom.us/docs/sdk/native-sdks/web/client-view/webinars#join-registered
-  // var registrantToken = "";
+  var registrantToken = "";
 
-  // function getSignature() {
-  //   // e.preventDefault();
+  function getSignature(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    e.preventDefault();
 
-  //   fetch(signatureEndpoint, {
-  //     method: "POST",
-  //     headers: { "Content-Type": "application/json" },
-  //     body: JSON.stringify({
-  //       meetingNumber: meetingNumber,
-  //       role: role,
-  //     }),
-  //   })
-  //     .then((res) => res.json())
-  //     .then((response) => {
-  //       startMeeting(response.signature);
-  //     })
-  //     .catch((error) => {
-  //       console.error(error);
-  //     });
-  // }
+    fetch(signatureEndpoint, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        meetingNumber: meetingNumber,
+        role: role,
+      }),
+    })
+      .then((res) => res.json())
+      .then((response) => {
+        // startMeeting(response.signature);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 
   // function startMeeting(signature: string) {
   //   document.getElementById("zmmtg-root").style.display = "block";
@@ -84,7 +84,7 @@ const MeetingPage = () => {
 
   return (
     <div>
-      <button>start meeting!!!!</button>
+      <button onClick={getSignature}>start meeting!!!!</button>
     </div>
   );
 };
